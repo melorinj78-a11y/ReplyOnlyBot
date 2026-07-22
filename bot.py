e7fa9157d3087e5babbdffe48b4d14f3e3fa01e0
melorinj78-a11y <melorinj78@gmail.com>
2026-07-22 22:16:21 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
+TOKEN = "YOUR_BOT_TOKEN"
+
+
+async def reply_only(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.effective_message
+
+    if message.reply_to_message:
+        return
+
+    try:
+        await message.delete()
+    except:
+        pass
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.ALL & (~filters.StatusUpdate.ALL), reply_only)
+)
+
+app.run_polling()
